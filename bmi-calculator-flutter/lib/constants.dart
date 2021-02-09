a351f71a0d984432f604a2f3219015f6481ef81e
Joe Brashear <joebrashear@live.com>
2021-02-09 08:34:46 -0500

    Added all other projects

---
+++ bmi-calculator-flutter/lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const kbottomContainerHeight = 80.0;
+const kActiveCardColour = Color(0xFF1D1E33);
+const kInactiveCardColour = Color(0xFFEB1555);
+const kBottomContainerColour = Color(0xFF111328);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kWidgetTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
